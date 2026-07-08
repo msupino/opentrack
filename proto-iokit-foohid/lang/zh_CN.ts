51f69962c6c76a40cd1f3f77e719af5b8b3e349f
Marco Supino <marco@drivenets.com>
2026-07-08 19:57:04 +0300

    lang: lupdate for PSVR tracker + touched proto strings

--- proto-iokit-foohid/lang/zh_CN.ts
+++ proto-iokit-foohid/lang/zh_CN.ts
@@ -1,4 +1,51 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
+<context>
+    <name>FooHIDJoystick</name>
+    <message>
+        <source>Failed to create virtual joystick</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to send values to virtual joystick</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IOKitProtocol</name>
+    <message>
+        <source>Load failure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IOKitProtocolDialog</name>
+    <message>
+        <source>No settings available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IOKitProtocolMetadata</name>
+    <message>
+        <source>Virtual joystick</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Unable to find FooHID IOService.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to connect to FooHID IOService.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
